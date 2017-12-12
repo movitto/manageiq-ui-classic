df4f4e57b5b15b27fec6c65877eba6f686953a0f
Mo Morsi <mmorsi@redhat.com>
2017-12-11 22:07:37 -0500

    BZ #1513053: resolve issue where vm smart scan option is incorrectly disabled
    This removed the call to 'super' in the vm_instance_template_scan toolbar
    button, since the call to @record.has_active_proxy? is sufficient to assertain
    if smart scan can be invoked. Fixes superclass SmartStateScan button to attempt
    to match records zone against target server zone if possible

--- app/helpers/application_helper/button/smart_state_scan.rb
+++ app/helpers/application_helper/button/smart_state_scan.rb
@@ -2,8 +2,12 @@ class ApplicationHelper::Button::SmartStateScan < ApplicationHelper::Button::Bas
   def check_smart_roles
     my_zone = MiqServer.my_server.my_zone
     MiqServer::ServerSmartProxy::SMART_ROLES.each do |role|
-      unless MiqServer.all.any? { |s| s.has_active_role?(role) && (s.my_zone == my_zone) }
-        @error_message = _("There is no server with the %{role_name} role enabled") % {:role_name => role}
+      unless MiqServer.all.any? { |s|
+        s.has_active_role?(role) &&
+        s.my_zone == (@record.respond_to?(:zone) ? @record.zone : my_zone)
+      }
+        @error_message = _("There is no server with the %{role_name} role enabled") %
+                         {:role_name => role}
       end
     end
   end
